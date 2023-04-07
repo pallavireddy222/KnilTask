191310b79396714296b538f28466d486d8704541
pallavireddy222 <120548430+pallavireddy222@users.noreply.github.com>
2023-04-07 17:07:06 +0530

    initial info

---
+++ CrudApiController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiApp.Models;
+
+namespace WebApiApp.Controllers
+{
+    public class CrudApiController : ApiController
+    {
+        EmployeeDBEntities EmpDB = new EmployeeDBEntities();
+        [HttpGet]
+        public IHttpActionResult GetContacts()
+        {
+            List<Contact> EmpContacts = EmpDB.Contacts.ToList().OrderByDescending(x => x.Id).ToList();
+            return Ok(EmpContacts);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetContactById(int Id)
+        {
+            var EmpContact = EmpDB.Contacts.Where(model => model.Id == Id).FirstOrDefault();
+            return Ok(EmpContact);
+        }
+        [HttpPost]
+        public IHttpActionResult ContactInsert(Contact con)
+        {
+            EmpDB.Contacts.Add(con);
+            EmpDB.SaveChanges();
+            return Ok();
+        }
+        [HttpPut]
+        public IHttpActionResult ContactUpdate(Contact con)
+        {
+            EmpDB.Entry(con).State = System.Data.Entity.EntityState.Modified;
+            EmpDB.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete]
+        public IHttpActionResult DelContactById(int Id)
+        {
+            var EmpContact = EmpDB.Contacts.Where(model => model.Id == Id).FirstOrDefault();
+            EmpDB.Entry(EmpContact).State = System.Data.Entity.EntityState.Deleted;
+            EmpDB.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ CrudMvcController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApiApp.Models;
+
+namespace WebApiApp.Controllers
+{
+    public class CrudMvcController : Controller
+    {
+
+        HttpClient client = new HttpClient();
+        // GET: CrudMvc
+        public ActionResult Index()
+        {
+            List<Contact> lstContacts = new List<Contact>();
+            client.BaseAddress = new Uri("http://localhost:49967/api/crudapi");
+            var response = client.GetAsync("CrudApi");
+            response.Wait();
+
+            var test = response.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Contact>>();
+                display.Wait();
+                lstContacts = display.Result;
+            }
+
+            return View(lstContacts);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Contact con)
+        {
+            client.BaseAddress = new Uri("http://localhost:49967/api/crudapi");
+            var response = client.PostAsJsonAsync<Contact>("CrudApi", con);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+        public ActionResult Details(int Id)
+        {
+            Contact con = null;
+            client.BaseAddress = new Uri("http://localhost:49967/api/crudapi");
+            var response = client.GetAsync("CrudApi?Id="+Id);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Contact>();
+                display.Wait();
+                con = display.Result;
+            }
+
+            return View(con);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            Contact con = null;
+            client.BaseAddress = new Uri("http://localhost:49967/api/crudapi");
+            var response = client.GetAsync("CrudApi?Id=" + Id);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Contact>();
+                display.Wait();
+                con = display.Result;
+            }
+
+            return View(con);
+        }
+        [HttpPost]
+        public ActionResult Edit(Contact con)
+        {
+            client.BaseAddress = new Uri("http://localhost:49967/api/crudapi");
+            var response = client.PutAsJsonAsync<Contact>("CrudApi", con);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            Contact con = null;
+            client.BaseAddress = new Uri("http://localhost:49967/api/crudapi");
+            var response = client.GetAsync("CrudApi?Id=" + Id);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Contact>();
+                display.Wait();
+                con = display.Result;
+            }
+
+            return View(con);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteContact(int Id)
+        {
+            client.BaseAddress = new Uri("http://localhost:49967/api/crudapi");
+            var response = client.DeleteAsync("CrudApi/"+Id);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+        }
+    }
+}
